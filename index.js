ccd5883a3c7c4db89c5daa480643eb273ab5cd22
Peter <petecruckshank@gmail.com>
2018-07-05 20:49:40 -0400

    added cid to return to check output

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("It begins");
+// based on the code from Free Code Camp
+
+function checkCashRegister(price, cash, cid) {
+    var change = cash - price;
+    // Here is your change, ma'am.
+    console.log(change);
+    return change + cid;
+  }
+  
+  // Example cash-in-drawer array:
+  // [["PENNY", 1.01],
+  // ["NICKEL", 2.05],
+  // ["DIME", 3.1],
+  // ["QUARTER", 4.25],
+  // ["ONE", 90],
+  // ["FIVE", 55],
+  // ["TEN", 20],
+  // ["TWENTY", 60],
+  // ["ONE HUNDRED", 100]]
+  
+  let answer = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  console.log(answer);
