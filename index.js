c0064afb9da6f8799a577a9a756562a976145b5e
Peter <petecruckshank@gmail.com>
2018-07-09 21:22:07 -0400

    got for...loop working for r_draw. May use fixed loop ?

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // based on the code from Free Code Camp
 
 function checkCashRegister(price, cash, cid) {
-    var change = cash - price;
+    const change = cash - price;
     // Here is your change, ma'am.
-    let drawCopy = new Map(cid.reverse());
+    let drawValue = new Map(cid.reverse());
     let totalCash = 0;
-    for (let val of drawCopy.values()){
+    for (let val of drawValue.values()){
       totalCash += Math.floor(val);
     }
     console.log(totalCash, change);
@@ -13,12 +13,30 @@ function checkCashRegister(price, cash, cid) {
       return NoDeal(cid);
     }
     else {
-      // 
-      let moneyBack = cid;
-      console.dir(moneyBack);
-      return {status: "OPEN", change: cid};
+      // new variable to subtract values from
+      const return_draw = cid;
+      // copy of change to subtract from
+      let change2 = change;
+
+      // loop through array until change = 0 
+      for (let [x, y] of return_draw) {
+        console.log(x, y);
+
+        // switch/case statement to subtract until change = 0
+        switch (x) {
+          case "ONE HUNDRED" :
+            let hundo = 100;
+            console.log(return_draw[x]);
+
+            y -= hundo;
+            change2 -= hundo;
+        }
+
+      // return open and pass back return_draw as .change
+      return {status: "OPEN", change: return_draw};
     }
   }
+}
 
   function NoDeal(cid) {
     return {status: "INSUFFICIENT_FUNDS", change: cid};
@@ -35,5 +53,5 @@ function checkCashRegister(price, cash, cid) {
   // ["TWENTY", 60],
   // ["ONE HUNDRED", 100]]
   
-  let answer = checkCashRegister(19.50, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  let answer = checkCashRegister(100, 200, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
   console.log(answer);
