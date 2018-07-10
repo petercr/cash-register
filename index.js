449dcd6d89aaeeec6f786c32e1f1de90d5c64c6b
Peter <petecruckshank@gmail.com>
2018-07-09 20:35:03 -0400

    got conditional && NoDeal() working

--- index.js
+++ index.js
@@ -3,16 +3,21 @@
 function checkCashRegister(price, cash, cid) {
     var change = cash - price;
     // Here is your change, ma'am.
-    let drawCopy = new Map(cid);
+    let drawCopy = new Map(cid.reverse());
     let totalCash = 0;
     for (let val of drawCopy.values()){
       totalCash += Math.floor(val);
     }
-    console.log(totalCash);
-    if (totalCash - change) {
-      NoDeal(cid);
+    console.log(totalCash, change);
+    if (totalCash < change) {
+      return NoDeal(cid);
     }
-    return {status: "OPEN", change: cid.reverse()};
+    else {
+      // 
+      let moneyBack = cid;
+      console.dir(moneyBack);
+      return {status: "OPEN", change: cid};
+    }
   }
 
   function NoDeal(cid) {
@@ -30,5 +35,5 @@ function checkCashRegister(price, cash, cid) {
   // ["TWENTY", 60],
   // ["ONE HUNDRED", 100]]
   
-  let answer = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  let answer = checkCashRegister(19.50, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
   console.log(answer);
