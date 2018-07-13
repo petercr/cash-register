602ba0e9e84517a1d4197f3bee94fcc07aef37c8
Peter Cruckshank <petecruckshank@gmail.com>
2018-07-13 17:43:46 -0400

    got both dollar and quarter amounts working Also switched from if/else to just if statements

--- index.js
+++ index.js
@@ -26,14 +26,30 @@ function checkCashRegister(price, cash, cid) {
           return_draw[0][1]-= 100;
           console.log(change, return_draw);
         }
-        else if (change / 50 >= 1) {
-          change -= 50;
-          return_draw[1][1] -= 50;
-        }
-        else if (change / 20 >= 1) {
+         if (change / 20 >= 1) {
           const times = Math.floor(change / 20);
           change -= times * 20;
-          return_draw -= times * 20;
+          return_draw[1][1] -= times * 20;
+        }
+         if (change / 10 >= 1) {
+          const times = Math.floor(change / 10);
+          change -= times * 10;
+          return_draw[2][1] -= times * 10;
+        }
+         if (change / 5 >= 1) {
+          const times = Math.floor(change / 5);
+          change -= times * 5;
+          return_draw[3][1] -= times * 5;
+        }
+         if (change / 1 >= 1) {
+          const times = Math.floor(change / 1);
+          change -= times;
+          return_draw[4][1] -= times;
+        }
+         if (change >= .25 ) {
+          const times = Math.floor(change / .25);
+          change -= (times * .25);
+          return_draw[5][1] -= (times * .25);
         }
        
         return giveBackChange(return_draw);
@@ -61,5 +77,5 @@ function checkCashRegister(price, cash, cid) {
   // ["TWENTY", 60],
   // ["ONE HUNDRED", 100]]
   
-  let answer = checkCashRegister(100, 200, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  let answer = checkCashRegister(5.25, 10, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
   console.log(answer);
